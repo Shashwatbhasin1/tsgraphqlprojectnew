23bac79274df710d47530baf590b08bb11dafa76
Shashwatbhasin1 <shashwatbhasin123@gmail.com>
2022-05-02 18:34:16 +0530

    first commit

---
+++ ts-gql/projectify/src/index.js
@@ -0,0 +1,39 @@
+const { ApolloServer, gql } = require('apollo-server');
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+    {
+    title: "harry potter",
+    author:"j.k. rowling"
+    },
+  ];
+
+const typeDefs = gql`
+ 
+  type Book {
+    title: String
+    author: String
+  }
+
+  
+  type Query {
+    books: [Book]
+  }
+`;
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+  const server = new ApolloServer({typeDefs,resolvers});
+  server.listen().then(({url}) => {
+         console.log(`server listening at ${url}`);
+  })
